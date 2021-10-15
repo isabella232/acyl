477757c62e2222a391eeb19604645def9c1387f6
Benjamen Keroack <benjamen@dollarshaveclub.com>
2021-10-15 14:22:32 -0700

    change qa_environment primary key and update fkey relationships

---
+++ migrations/0025_change_env_primary_key_down.sql
@@ -0,0 +1,20 @@
+DROP INDEX IF EXISTS idx_qa_environments_name;
+ALTER TABLE qa_environments DROP CONSTRAINT qa_environments_pkey;
+
+DROP INDEX IF EXISTS idx_helm_releases_envname;
+ALTER TABLE helm_releases
+    DROP CONSTRAINT helm_releases_env_id_fkey;
+ALTER TABLE helm_releases
+    ADD CONSTRAINT helm_releases_env_name_fkey FOREIGN KEY (env_name) REFERENCES qa_environments (name);
+ALTER TABLE helm_releases
+    DROP COLUMN environment_id;
+
+DROP INDEX IF EXISTS idx_kubernetes_environments_envname;
+ALTER TABLE kubernetes_environments
+    DROP CONSTRAINT kubernetes_environments_env_id_fkey;
+ALTER TABLE kubernetes_environments
+    ADD CONSTRAINT kubernetes_environments_env_name_fkey FOREIGN KEY (env_name) REFERENCES qa_environments (name);
+ALTER TABLE kubernetes_environments
+    DROP COLUMN environment_id;
+
+ALTER TABLE qa_environments DROP COLUMN id;
---
+++ migrations/0025_change_env_primary_key_up.sql
@@ -0,0 +1,28 @@
+-- stop using environment name as primary key for environments instead use numeric id
+-- we have to also change all foreign key relationships
+
+ALTER TABLE qa_environments ADD COLUMN id bigserial UNIQUE NOT NULL;
+
+--drop constraint on foreign key tables and set new fk
+ALTER TABLE kubernetes_environments ADD COLUMN environment_id bigint;
+ALTER TABLE kubernetes_environments DROP CONSTRAINT kubernetes_environments_env_name_fkey;
+UPDATE kubernetes_environments T SET environment_id = (SELECT id FROM qa_environments WHERE name = T.env_name);
+ALTER TABLE kubernetes_environments
+    ADD CONSTRAINT kubernetes_environments_env_id_fkey FOREIGN KEY (environment_id) REFERENCES qa_environments (id) ON DELETE CASCADE;
+CREATE UNIQUE INDEX IF NOT EXISTS idx_kubernetes_environments_envname ON kubernetes_environments
+    ( env_name );
+
+ALTER TABLE helm_releases ADD COLUMN environment_id bigint;
+ALTER TABLE helm_releases DROP CONSTRAINT helm_releases_env_name_fkey;
+UPDATE helm_releases T SET environment_id = (SELECT id FROM qa_environments WHERE name = T.env_name);
+ALTER TABLE helm_releases
+    ADD CONSTRAINT helm_releases_env_id_fkey FOREIGN KEY (environment_id) REFERENCES qa_environments (id) ON DELETE CASCADE;
+CREATE UNIQUE INDEX IF NOT EXISTS idx_helm_releases_envname ON helm_releases
+    ( env_name );
+
+-- -- change primary key on qa_environments
+ALTER TABLE qa_environments DROP CONSTRAINT qa_environments_pkey;
+ALTER TABLE qa_environments ADD PRIMARY KEY (id);
+ALTER TABLE qa_environments ADD UNIQUE (name);
+CREATE UNIQUE INDEX IF NOT EXISTS idx_qa_environments_name ON qa_environments
+    ( name );
